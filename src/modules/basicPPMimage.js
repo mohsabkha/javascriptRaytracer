e36c9cb864dda89e0a07420b2099c8bd1e14d22c
Mohammad Sabir Khan <mohsabkha@gmail.com>
2021-12-27 19:23:49 +0500

    added documentation

--- src/modules/basicPPMimage.js
+++ src/modules/basicPPMimage.js
@@ -1,18 +1,25 @@
+// node fs module used for writing files
 const fs = require('fs');
 
 const basicPPMimage = () => {
+    // determine the width, and the numbeer of subsequent columns to be made
     let imageSizeX = 200;
+    // determine the height, and the number of subsequent rows to be made
     let imageSizeY = 100;
+    // initializing the ppm file with type, width, and higher, along with rgb values
     fs.writeFileSync(`img.ppm`,`P3\n` + `${imageSizeX} ${imageSizeY}` + `\n255\n`, err => {});
+    // creating the rows
     for(let j = imageSizeY-1; j >= 0; j--){
+        // iterating over every column in the row and generating pixel
         for(let i = 0; i < imageSizeX; i++){
             let red = i / imageSizeX;
             let green = j / imageSizeY;
             let blue = 0.2;
-
-            let iRed = Math.ceil(255.99*red);
-            let iGreen = Math.ceil(255.99*green);
-            let iBlue = Math.ceil(255.99*blue);
+            // floor or ceiling could have been used
+            let iRed = Math.floor(255.99*red);
+            let iGreen = Math.floor(255.99*green);
+            let iBlue = Math.floor(255.99*blue);
+            // writing pixel to ppm file
             fs.writeFileSync(`img.ppm`, `${iRed} ${iGreen} ${iBlue}\n`, { flag: 'a' }, err => {});
         }
     }
